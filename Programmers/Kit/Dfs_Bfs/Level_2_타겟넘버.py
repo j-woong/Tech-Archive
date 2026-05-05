a9445d0cd1998529eb964c86d52b47a270d4aa0f
웅이에요 <woooongjee@MacBook-Air.local>
2026-05-05 16:53:41 +0900

    Solve: 프로그래머스 Level_Tests/Level_2 타겟넘버 (DFS)재풀이

--- Programmers/Kit/Dfs_Bfs/Level_2_타겟넘버.py
+++ Programmers/Kit/Dfs_Bfs/Level_2_타겟넘버.py
@@ -1,4 +1,15 @@
+"""
+***로직***
+1. 입력 변수: 
+- numbers: 숫자 배열
+- target: 최종적으로 만들 숫자
+2. 핵심 변수:
+- cur_n: 현재 값을 저장하여 사용
+- depth: 숫자를 더한 횟수로 사용할 변수
+
+"""
 def solution(numbers, target):
+    answer = 0
 
     def dfs(cur_n,depth):
         if depth == len(numbers):
@@ -7,15 +18,7 @@ def solution(numbers, target):
             else:
                 return 0
             
-        return dfs(cur_n + numbers[depth],depth + 1) + dfs(cur_n - numbers[depth],depth + 1)
-        
+        return dfs(cur_n + numbers[depth],depth+1) + dfs(cur_n - numbers[depth],depth+1)
+            
     return dfs(0,0)
-
-"""
-from itertools import product
-def solution(numbers, target):
-    l = [ (x,-x) for x in numbers]
-    s = [list(map(sum,product(*l)))]
-
-    return s.count(target)
-"""
+        
