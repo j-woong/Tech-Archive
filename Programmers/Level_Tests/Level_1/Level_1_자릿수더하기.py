2d7621276c66ff3b95e6ddd2029224f4df65d6ad
(김지웅) <(rlawldnd905@naver.com)>
2026-05-31 14:25:51 +0900

    Solve: 프로그래머스 Level_Tests/Level_1 자릿수더하기 (구현)

---
+++ Programmers/Level_Tests/Level_1/Level_1_자릿수더하기.py
@@ -0,0 +1,13 @@
+def solution(n):    
+    answer = 0
+    while n >= 10:
+        answer += n % 10
+        n //= 10
+    answer += n
+    return answer
+
+"""
+def solution(n):    
+    return sum(map(int,str(n)))
+
+"""
